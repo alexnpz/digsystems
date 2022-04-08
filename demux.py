2200cc859962739aa080252535c157f4a63da784
alexnpz <alex.noste@outlook.es>
2022-04-08 12:28:44 +0100

    Demux altered script

--- demux.py
+++ demux.py
@@ -9,13 +9,13 @@ def validations (input):
         input = int(input)
     except:
         raise SystemExit ('The input parameters must be integer numbers, exiting program...')
+    if input <= 0 :
+        raise SystemExit('The selector must be greater than 0')
     return int(input)
 
 inputs = random.randint(0,1000)
 output_selector = input('Introduce the number of selectors :')
 output_selector = validations(output_selector)
-if output_selector <= 0 :
-    raise SystemExit('The selector must be greater than 0')
 output_channels = 2**int(output_selector)
 dictchannout = {i:[] for i in range(output_channels)}
 print(dictchannout)
@@ -23,6 +23,9 @@ print(dictchannout)
 while True:
     channel_selected = input(f'Introduce the channel that will be the output (from 0 to {output_channels-1}) or q to exit the program: ')
     channel_selected = validations(channel_selected)
-    dictchannout [channel_selected].append(inputs)
-    print(dictchannout)
+    if channel_selected > output_channels - 1 : 
+        print ('Error, channel selected value must be lesser than the output value')
+    else:
+        dictchannout [channel_selected].append(inputs)
+        print(dictchannout)
     inputs = random.randint(0,1000)
