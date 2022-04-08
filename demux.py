c2fa551d81fd7dd85dc072236f4b50b563bbd002
alexnpz <alex.noste@outlook.es>
2022-04-08 12:18:44 +0100

    Demux altered script

--- demux.py
+++ demux.py
@@ -1,7 +1,7 @@
 # Implementation of a demultiplexer
-# Notice that if a channel selected with previous data saved is selected again, the data will be destroy. This can be improved by saving in a different way
+# Use of a dictionary to keep old values and not replace them with every iteration
+# Use of dict comprehension
 import random
-
 def validations (input):
     if input == 'q':
         raise SystemExit ('Exit Program')
@@ -17,11 +17,12 @@ output_selector = validations(output_selector)
 if output_selector <= 0 :
     raise SystemExit('The selector must be greater than 0')
 output_channels = 2**int(output_selector)
-listchannout = [0 for i in range(output_channels)]
+dictchannout = {i:[] for i in range(output_channels)}
+print(dictchannout)
 
 while True:
     channel_selected = input(f'Introduce the channel that will be the output (from 0 to {output_channels-1}) or q to exit the program: ')
     channel_selected = validations(channel_selected)
-    listchannout.insert(channel_selected,inputs)
-    print(listchannout)
+    dictchannout [channel_selected].append(inputs)
+    print(dictchannout)
     inputs = random.randint(0,1000)
